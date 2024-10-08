e3ed41916e96c3251502423bdcd7f7952d8aba5e
DimaE-g-o-r <egorovkog@mail.ru>
2024-10-08 21:46:12 +0500

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -3,9 +3,10 @@ def get_matrix(n,m,value):
     for i in range(n):
         t = [value]*m
         matrix.append(t)
-        return matrix
         if i <= 0:
             continue
+        return matrix
+
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
